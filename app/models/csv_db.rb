62ca21c32494c30922de828dc6bdd7c7b4f3daf5
socjopata <socjopata@gmail.com>
2013-06-26 16:59:10 +0200

    Initial AR transaction wrapping and very basic error handling

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -1,18 +1,21 @@
 require 'csv'
+require 'csv'
 class CsvDb
   class << self
     def convert_save(target_model, csv_data, options = {}, &block)
       options = { headers: true, header_converters: :symbol }.merge(options)
       csv_file = csv_data.read
-      CSV.parse(csv_file, options) do |row|
-        data = row.to_hash
-        if data.present?
-          if (block_given?)
-             block.call(target_model, data)
-           else
-             target_model.create!(data)
-           end
-         end
+      ActiveRecord::Base.transaction do
+        CSV.parse(csv_file, options) do |row|
+          data = row.to_hash
+          if data.present?
+            if (block_given?)
+              block.call(target_model, data)
+            else
+              target_model.create!(data)
+            end
+          end
+        end
       end
     end
   end

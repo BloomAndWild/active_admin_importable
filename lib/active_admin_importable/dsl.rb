e816158f37b5d9b60339fb998f441500862114cd
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2015-09-17 14:29:06 +0200

    Wording, syntax, etc.

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -1,31 +1,21 @@
 module ActiveAdminImportable
   module DSL
-    def active_admin_importable(options={}, partials={}, &block)
-      action_item :only => :index do
-        link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_csv'
+    def active_admin_importable(options = {}, partials = {}, &block)
+      action_item :upload_csv, only: :index do
+        link_to "CSV Import", action: 'upload_csv'
       end
 
       collection_action :upload_csv do
-        render "admin/csv/upload_csv", :locals => {:partials => partials}
+        render "admin/csv/upload_csv", locals: { partials: partials,
+          columns: options.delete(:columns) || [] }
       end
 
-      collection_action :import_csv, :method => :post do
-        begin
-          CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], options, &block)
-        rescue => exception
-          @something_went_wrong = true
-        else
-          @something_went_wrong = false
-        end
-
-        unless  @something_went_wrong
-          flash[:notice] = "#{active_admin_config.resource_name.to_s} imported successfully!"
-          redirect_to :action => :index
-        else
-          flash[:error] = "Something went wrong!"
-          redirect_to :action => :index
-        end
+      collection_action :import_csv, method: :post do
+        CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], options, &block)
+        redirect_to action: :index, notice: "CSV imported successfully!"
       end
     end
+
+    alias_method :csv_import, :active_admin_importable
   end
 end

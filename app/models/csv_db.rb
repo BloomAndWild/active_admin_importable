0f0923d3b9e914f7a6bfe1f84d67dfa236396384
Bart <bartosz@kopinski.pl>
2019-07-06 15:29:39 +0200

    Fix for mutable options

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -2,18 +2,16 @@ require 'csv'
 class CsvDb
   class << self
     def convert_save_csv(csv_data, options = {}, &block)
-      options = { headers: true, header_converters: :symbol }.merge(options.except(:columns))
       csv_file = csv_data.read
       ActiveRecord::Base.transaction do
-        block.call(CSV.parse(csv_file, options))
+        block.call(CSV.parse(csv_file, csv_options(options)))
       end
     end
 
     def convert_save(target_model, csv_data, options = {}, &block)
-      options = { headers: true, header_converters: :symbol }.merge(options.except(:columns))
       csv_file = csv_data.read
       ActiveRecord::Base.transaction do
-        CSV.parse(csv_file, options) do |row|
+        CSV.parse(csv_file, csv_options(options)) do |row|
           data = row.to_hash
           if data.present? && data.any?
             if (block_given?)
@@ -24,6 +22,12 @@ class CsvDb
           end
         end
       end
+    end
+
+    private
+
+    def csv_options(options)
+      { headers: true, header_converters: :symbol }.merge(options.except(:columns, :import_method))
     end
   end
 end
--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -4,7 +4,7 @@ module ActiveAdminImportable
       importable_action_setup(options, partials)
 
       collection_action :import_csv, method: :post do
-        if options.delete(:import_method) == :full
+        if options[:import_method] == :full
           CsvDb.convert_save_csv(params[:dump][:file], options, &block)
         else
           CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], options, &block)
